0bde00e60a08d21a1dafdf24f72859df19e68ab0
Singorenko <cbaezenko@gmail.com>
2019-02-27 17:24:32 +0300

    add image location for the recycler view categories.

--- app/src/main/java/com/singorenko/miofondo/model/CategoryList.kt
+++ app/src/main/java/com/singorenko/miofondo/model/CategoryList.kt
@@ -7,9 +7,13 @@ class CategoryList {
 
     fun getCategory () : ArrayList<CategoryModel> {
         categoryList.add(CategoryModel("Chile", R.drawable.ic_launcher_background))
-        categoryList.add(CategoryModel("Espana", R.drawable.ic_launcher_background))
+        categoryList.add(CategoryModel("Spain", R.drawable.ic_launcher_background))
         categoryList.add(CategoryModel("Rusia", R.drawable.ic_launcher_background))
         categoryList.add(CategoryModel("USA", R.drawable.ic_launcher_background))
+        categoryList.add(CategoryModel("Argentina", R.drawable.ic_launcher_background))
+        categoryList.add(CategoryModel("Mexico", R.drawable.ic_launcher_background))
+        categoryList.add(CategoryModel("China", R.drawable.ic_launcher_background))
+        categoryList.add(CategoryModel("Japon", R.drawable.ic_launcher_background))
         categoryList.add(CategoryModel("India", R.drawable.ic_launcher_background))
         categoryList.add(CategoryModel("Heraldica", R.drawable.ic_launcher_background))
         return  categoryList
--- app/src/main/java/com/singorenko/miofondo/view/GridCategoryAdapter.kt
+++ app/src/main/java/com/singorenko/miofondo/view/GridCategoryAdapter.kt
@@ -1,17 +1,22 @@
 package com.singorenko.miofondo.view
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.singorenko.miofondo.R
 import com.singorenko.miofondo.model.CategoryModel
+import com.singorenko.miofondo.model.categoryList
+import kotlinx.android.synthetic.main.item_category.view.*
 import kotlinx.android.synthetic.main.item_inside_category.view.*
 
-class GridCategoryAdapter (val categoryList: ArrayList<CategoryModel>, val context: Context) :
-    RecyclerView.Adapter<GridCategoryViewHolder>(){
+class GridCategoryAdapter(private val categoryList: ArrayList<CategoryModel>, private val context: Context) :
+    RecyclerView.Adapter<GridCategoryAdapter.GridCategoryViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridCategoryViewHolder {
         return GridCategoryViewHolder(
             LayoutInflater.from(context).inflate(
@@ -28,10 +33,23 @@ class GridCategoryAdapter (val categoryList: ArrayList<CategoryModel>, val conte
 
     override fun onBindViewHolder(holder: GridCategoryViewHolder, position: Int) {
         holder.tvCategoryTitle.text = categoryList[position].title
+        holder.ivCategoryImage.setImageDrawable(context.resources.getDrawable(categoryList[position].image))
     }
-}
 
 
-class GridCategoryViewHolder (view: View) : RecyclerView.ViewHolder(view){
-    var tvCategoryTitle: TextView = view.tv_category_title
+    class GridCategoryViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        override fun onClick(v: View?) {
+            val clickedPosition = adapterPosition
+            Log.d("TAG", categoryList[clickedPosition].title+ clickedPosition)
+            //TODO refresh screen with images of the selected category
+        }
+
+        var cvCategory: CardView = view.cv_category
+        var tvCategoryTitle: TextView = view.tv_category_title
+        var ivCategoryImage: ImageView = view.iv_category_image
+
+        init {
+            cvCategory.setOnClickListener(this)
+        }
+    }
 }
