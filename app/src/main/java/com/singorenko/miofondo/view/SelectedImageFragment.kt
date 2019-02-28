44ce92467828ca7ca9a3e5a470f1dde0a8f1e274
Singorenko <cbaezenko@gmail.com>
2019-02-28 15:26:50 +0300

    Add functionally for wallpaper set

--- app/src/main/java/com/singorenko/miofondo/view/SelectedImageFragment.kt
+++ app/src/main/java/com/singorenko/miofondo/view/SelectedImageFragment.kt
@@ -1,5 +1,8 @@
 package com.singorenko.miofondo.view
 
+import android.app.WallpaperManager
+import android.graphics.Bitmap
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,7 +11,9 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.singorenko.miofondo.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_selected_image.*
+import java.io.IOException
 
 class SelectedImageFragment : Fragment() {
 
@@ -29,6 +34,29 @@ class SelectedImageFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         iv_selected_image.setOnClickListener {
             Log.d("TAG", "selected the image to use as background")
+
+            doAsync{
+                setWallpaper("https://raw.githubusercontent.com/square/picasso/master/website/static/sample.png")
+            }.execute()
+
+//
+//            var wallpaperImage: Bitmap = Picasso.get().load("https://raw.githubusercontent.com/square/picasso/master/website/static/sample.png").get()
+//            var mWallpaperManager: WallpaperManager = WallpaperManager.getInstance(context)
+//            try {
+//                mWallpaperManager.setBitmap(wallpaperImage)
+//            } catch (e: IOException){
+//                e.printStackTrace()
+//            }
+        }
+    }
+
+    fun setWallpaper(urlImage: String){
+        val wallpaperImage: Bitmap = Picasso.get().load(urlImage).get()
+        val mWallpaperManager: WallpaperManager = WallpaperManager.getInstance(context)
+        try {
+            mWallpaperManager.setBitmap(wallpaperImage)
+        } catch (e: IOException){
+            e.printStackTrace()
         }
     }
 
@@ -40,5 +68,12 @@ class SelectedImageFragment : Fragment() {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+     class doAsync(val handler: ()-> Unit) : AsyncTask<Void, Void, Void>(){
+         override fun doInBackground(vararg params: Void?): Void? {
+             handler()
+             return null
+         }
     }
 }
